998afb52245a184e494d286a212ef8e981af0945
Samir Ibradžić <sibradzic@gmail.com>
2014-01-07 21:49:10 +0900

    Add Custom MMS User-Agent configuration option
    More than few carriers restrict MMS messages on their nework by filtering MMS client User-Agent strings. Some notorious carriers even preven MMS from being sent if the UA sting does not match their own obscure list.
    This add additional settings option, User-Agent override, with the ability to select few popular User-Agent strings, as well as typing in a custom information.
    
    Change-Id: I5a68827039c815f159773c4aad96063a0d98d9e4
    
    Conflicts:
    	res/values/arrays.xml
    	res/xml/preferences.xml
    
    Conflicts:
    	res/xml/preferences.xml
    	src/com/android/mms/transaction/HttpUtils.java
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/transaction/HttpUtils.java
+++ src/com/android/mms/transaction/HttpUtils.java
@@ -37,7 +37,9 @@ import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.http.AndroidHttpClient;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Config;
@@ -71,6 +73,7 @@ public class HttpUtils {
     // Definition for necessary HTTP headers.
     private static final String HDR_KEY_ACCEPT = "Accept";
     private static final String HDR_KEY_ACCEPT_LANGUAGE = "Accept-Language";
+    private static String mUserAgent;
 
     private static final String HDR_VALUE_ACCEPT =
         "*/*, application/vnd.wap.mms-message, application/vnd.wap.sic";
@@ -103,6 +106,7 @@ public class HttpUtils {
             Log.v(TAG, "httpConnection: params list");
             Log.v(TAG, "\ttoken\t\t= " + token);
             Log.v(TAG, "\turl\t\t= " + url);
+            Log.v(TAG, "\tUser-Agent\t\t=" + mUserAgent);
             Log.v(TAG, "\tmethod\t\t= "
                     + ((method == HTTP_POST_METHOD) ? "POST"
                             : ((method == HTTP_GET_METHOD) ? "GET" : "UNKNOWN")));
@@ -300,8 +304,15 @@ public class HttpUtils {
     }
 
     private static AndroidHttpClient createHttpClient(Context context) {
-        String userAgent = MmsConfig.getUserAgent();
-        AndroidHttpClient client = AndroidHttpClient.newInstance(userAgent, context);
+        // Get Shared Preferences and User Defined User Agent for MMS
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        mUserAgent = prefs.getString(MessagingPreferenceActivity.USER_AGENT, MmsConfig.getUserAgent());
+        if (mUserAgent == null || mUserAgent.equals("") || mUserAgent.equals("default")) {
+            mUserAgent = MmsConfig.getUserAgent();
+        } else if (mUserAgent.equals("custom")) {
+            mUserAgent = prefs.getString(MessagingPreferenceActivity.USER_AGENT_CUSTOM, MmsConfig.getUserAgent());
+        }
+        AndroidHttpClient client = AndroidHttpClient.newInstance(mUserAgent, context);
         HttpParams params = client.getParams();
         HttpProtocolParams.setContentCharset(params, "UTF-8");
 
@@ -310,7 +321,7 @@ public class HttpUtils {
 
         if (Log.isLoggable(LogTag.TRANSACTION, Log.DEBUG)) {
             Log.d(TAG, "[HttpUtils] createHttpClient w/ socket timeout " + soTimeout + " ms, "
-                    + ", UA=" + userAgent);
+                    + ", UA=" + mUserAgent);
         }
         HttpConnectionParams.setSoTimeout(params, soTimeout);
         return client;
